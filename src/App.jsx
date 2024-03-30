1f207e3ecb9d42ea65683ceb988d4a4cca90efa2
Swagatika <mswagatika3009@gmail.com>
2024-03-30 20:08:10 +0530

    All button functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./App.css";
 
 const pomoSuggetions = [
   { focusTime: 25, breakTime: 5 },
   { focusTime: 50, breakTime: 10 },
-  { focusTime: 90, breakTime: 30 },
+  { focusTime: 1, breakTime: 30 },
 ];
 
 function App() {
@@ -12,28 +12,80 @@ function App() {
     Focus: { minutes: 25, seconds: 0 },
     Break: { minutes: 5, seconds: 0 },
   });
+  const timerRef = useRef(timer);
   //2 Timer sates -> Focus, Break
   const [timerState, setTimerState] = useState("Focus");
   let min = timer[timerState].minutes;
   let sec = timer[timerState].seconds;
-  //3 states -> Start, Countdown, Pause
-  const [state, setState] = useState("");
+  //states -> Start, Countdown, Pause
+  const [state, setState] = useState("Start");
+  const timerCountdown = useRef(null);
   const suggetionButtons = pomoSuggetions.map((pomodoro, index) => {
     return (
       <button
         key={index}
         className="sugButtons"
-        onClick={() =>
-          setTimer({
+        onClick={() => {
+          let timerObj = {
             Focus: { minutes: pomodoro.focusTime, seconds: 0 },
             Break: { minutes: pomodoro.breakTime, seconds: 0 },
-          })
-        }
+          };
+          setTimer(timerObj);
+          timerRef.current = timerObj;
+          stopTimer();
+        }}
       >
         {pomodoro.focusTime}/{pomodoro.breakTime}
       </button>
     );
   });
+  let stateChangingBtn = () => {
+    if (state === "Start") return "Start";
+    else if (state === "Countdown") return "Pause";
+    else if (state === "Pause") return "Restart";
+  };
+  function handleTimer() {
+    if (state == "Start" || state == "Pause") {
+      setState("Countdown");
+      timerCountdown.current = setInterval(() => {
+        setTimer((prevTime) => {
+          let min = prevTime[timerState].minutes;
+          let sec = prevTime[timerState].seconds;
+          if (min === 0 && sec === 0) {
+            clearInterval(timerCountdown.current);
+            setState("Start");
+            return timerRef.current;
+          } else if (sec === 0) {
+            return {
+              ...prevTime,
+              [timerState]: {
+                minutes: min - 1,
+                seconds: 59,
+              },
+            };
+          } else {
+            return {
+              ...prevTime,
+              [timerState]: {
+                ...prevTime[timerState],
+                seconds: prevTime[timerState].seconds - 1,
+              },
+            };
+          }
+        });
+      }, 1000);
+    } else if (state == "Countdown") {
+      setState("Pause");
+      clearInterval(timerCountdown.current);
+    }
+  }
+  function stopTimer() {
+    if (timerCountdown.current !== null) {
+      clearInterval(timerCountdown.current);
+      setState("Start");
+      setTimer(timerRef.current);
+    }
+  }
   return (
     <>
       <div className="container">
@@ -43,15 +95,27 @@ function App() {
             {min < 10 ? "0" + min.toString() : min}:
             {sec < 10 ? "0" + sec.toString() : sec}
           </h1>
-          <button type="reset" id="resetButton">
-            Start/Pause
+          <button type="button" id="resetButton" onClick={handleTimer}>
+            {stateChangingBtn()}
           </button>
         </div>
         <div className="buttonContainer">
-          <button type="button" onClick={() => setTimerState("Focus")}>
+          <button
+            type="button"
+            onClick={() => {
+              setTimerState("Focus");
+              stopTimer();
+            }}
+          >
             Focus
           </button>
-          <button type="button" onClick={() => setTimerState("Break")}>
+          <button
+            type="button"
+            onClick={() => {
+              setTimerState("Break");
+              stopTimer();
+            }}
+          >
             Break
           </button>
         </div>
